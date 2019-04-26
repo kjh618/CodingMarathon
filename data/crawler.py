106208180a88875df60d9a897e810e77ccc19897
Jihoon Kim <kjh618@kaist.ac.kr>
2019-04-26 22:46:04 +0900

    Implement crawler.py and crawl data

--- data/crawler.py
+++ data/crawler.py
@@ -1,11 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-f = open('gun_list.txt', 'r')
+f = open('data/gun_list.txt', 'r')
 gun_list = []
 for line in f:
     gun_list.append(line.strip())
 f.close()
 
-r = requests.get('https://pubg.gamepedia.com/AKM')
-soup = BeautifulSoup(r.text, 'html.parser')
+out = open('data/gun_data.txt', 'w')
+
+for gun in gun_list:
+    print(gun + ': ', end='')
+
+    gun_url = gun.replace(' ', '_')
+    r = requests.get('https://pubg.gamepedia.com/' + gun_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    overview = soup.find('th', string='Overview\n')
+    rows = overview.parent.find_next_siblings('tr')
+
+    out.write(gun + '\n')
+    for row in rows:
+        if row.find().name == 'th':
+            break
+        else:
+            pair = row.find_all('td')
+            if pair[0].string != None and pair[1].string != None:
+                out.write(pair[0].string.strip() + ': ' + pair[1].string.strip() + '\n')
+    out.write('---\n')
+
+    print('Done')
+
+out.close()
